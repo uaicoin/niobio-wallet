187a3b5396fcb4fd84097c652ad78312c05c13bd
Helder Garcia <helder.garcia@gmail.com>
2018-01-15 16:38:55 -0200

    Change copyrights header

--- src/PaymentServer.cpp
+++ src/PaymentServer.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
-// Copyright (c) 2016 The Karbowanec developers
+// Copyright (c) 2016 - 2018 Niobio developers - Derived work from -Karbowanec-
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
